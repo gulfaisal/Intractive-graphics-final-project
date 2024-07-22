2a9c89d35d5285d7c367f97dcb0296221d55cc94
gulfaisal <144256733+gulfaisal@users.noreply.github.com>
2024-07-22 17:06:10 +0500

    Final Push

---
+++ js/PointerLockControls.js
@@ -0,0 +1,367 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+THREE.PointerLockControls = function (camera) {
+  var scope = this;
+
+  camera.target = new THREE.Vector3(0, 0, 0);
+  scope.lookVertical = true;
+  scope.lon = -90; // initial camera direction
+  scope.lat = 0;
+  scope.dxSpeed = 0.1;
+  scope.dySpeed = 0.3;
+  scope.verticalMin = 0;
+  scope.verticalMax = Math.PI;
+
+  scope.movementSpeed = 1.0;
+  scope.blocker = document.getElementById("blocker");
+  scope.instructions = document.getElementById("instructions");
+
+  var moveForward = false;
+  var moveBackward = false;
+  var moveLeft = false;
+  var moveRight = false;
+  var moveUp = false;
+  var moveDown = false;
+
+  var isOnObject = false;
+  var canJump = false;
+
+  var velocity = new THREE.Vector3();
+
+  var PI_2 = Math.PI / 2;
+
+  var onMouseMove = function (event) {
+    if (scope.enabled === false) return;
+
+    var movementX =
+      event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+    var movementY =
+      event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+    //yawObject.rotation.y -= movementX * 0.002;
+    //pitchObject.rotation.x -= movementY * 0.002;
+    mouseDelta(movementX, movementY);
+
+    //pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
+  };
+
+  var mouseDelta = function (dx, dy) {
+    scope.lon += dx * scope.dxSpeed;
+    if (scope.lookVertical) scope.lat -= dy * scope.dySpeed;
+
+    scope.lat = Math.max(-85, Math.min(85, scope.lat));
+    scope.phi = THREE.Math.degToRad(90 - scope.lat);
+
+    scope.theta = THREE.Math.degToRad(scope.lon);
+
+    if (scope.constrainVertical) {
+      scope.phi = THREE.Math.mapLinear(
+        scope.phi,
+        0,
+        Math.PI,
+        scope.verticalMin,
+        scope.verticalMax
+      );
+    }
+
+    position = camera.position;
+
+    camera.target.x =
+      position.x + 100 * Math.sin(scope.phi) * Math.cos(scope.theta);
+    camera.target.y = position.y + 100 * Math.cos(scope.phi);
+    camera.target.z =
+      position.z + 100 * Math.sin(scope.phi) * Math.sin(scope.theta);
+
+    camera.lookAt(camera.target);
+  };
+
+  var onKeyDown = function (event) {
+    switch (event.keyCode) {
+      case 38: // up
+      case 87: // w
+        moveForward = true;
+        break;
+
+      case 37: // left
+      case 65: // a
+        moveLeft = true;
+        break;
+
+      case 40: // down
+      case 83: // s
+        moveBackward = true;
+        break;
+
+      case 39: // right
+      case 68: // d
+        moveRight = true;
+        break;
+
+      case 32: /*space*/
+      case 82:
+        /*R*/ moveUp = true;
+        break;
+      case 70:
+        /*F*/ moveDown = true;
+        break;
+    }
+  };
+
+  var onKeyUp = function (event) {
+    switch (event.keyCode) {
+      case 38: // up
+      case 87: // w
+        moveForward = false;
+        break;
+
+      case 37: // left
+      case 65: // a
+        moveLeft = false;
+        break;
+
+      case 40: // down
+      case 83: // a
+        moveBackward = false;
+        break;
+
+      case 39: // right
+      case 68: // d
+        moveRight = false;
+        break;
+
+      case 32: /*space*/
+      case 82:
+        /*R*/ moveUp = false;
+        break;
+      case 70:
+        /*F*/ moveDown = false;
+        break;
+    }
+  };
+
+  document.addEventListener("mousemove", onMouseMove, false);
+  document.addEventListener("keydown", onKeyDown, false);
+  document.addEventListener("keyup", onKeyUp, false);
+
+  this.enabled = false;
+
+  /*this.getObject = function () {
+
+        return yawObject;
+
+    };*/
+
+  this.isOnObject = function (boolean) {
+    isOnObject = boolean;
+    canJump = boolean;
+  };
+
+  this.update = function (delta) {
+    if (scope.enabled === false) return;
+
+    /*velocity.x += ( - velocity.x ) * 0.08 * delta;
+        velocity.z += ( - velocity.z ) * 0.08 * delta;
+
+        velocity.y -= 0.25 * delta;*/
+
+    velocity.x = 0;
+    velocity.y = 0;
+    velocity.z = 0;
+
+    if (moveForward) velocity.z -= scope.movementSpeed * delta;
+    if (moveBackward) velocity.z += scope.movementSpeed * delta;
+
+    if (moveLeft) velocity.x -= scope.movementSpeed * delta;
+    if (moveRight) velocity.x += scope.movementSpeed * delta;
+
+    if (isOnObject === true) {
+      velocity.y = Math.max(0, velocity.y);
+    }
+
+    camera.vx = velocity.x;
+    //camera.vy = velocity.y;
+    camera.vz = velocity.z;
+
+    if (this.fly) {
+      if (moveUp) camera.translateY(g.jumpVY);
+      if (moveDown) camera.translateY(-g.jumpVY);
+    } else {
+      // g force = jump_speed * 0.5 / max_jump_height
+      if (moveUp && camera.isOnGround) {
+        camera.isOnGround = false;
+        camera.vy = g.jumpVY;
+      }
+    }
+
+    /*if ( yawObject.position.y < 10 ) {
+
+            velocity.y = 0;
+            yawObject.position.y = 10;
+
+            canJump = true;
+
+        }*/
+  };
+
+  scope.onResume = function () {
+    scope.enabled = true;
+    scope.blocker.style.display = "none";
+  };
+
+  scope.onPause = function () {
+    scope.enabled = false;
+
+    scope.blocker.style.display = "-webkit-box";
+    scope.blocker.style.display = "-moz-box";
+    scope.blocker.style.display = "box";
+
+    scope.instructions.style.display = "";
+  };
+
+  this.handleResize = function () {};
+
+  this.register = function () {
+    element = document.body;
+
+    var pointerlockchange = function (event) {
+      if (
+        document.pointerLockElement === element ||
+        document.mozPointerLockElement === element ||
+        document.webkitPointerLockElement === element
+      ) {
+        scope.onResume();
+      } else {
+        scope.onPause();
+      }
+    };
+
+    var pointerlockerror = function (event) {
+      scope.instructions.style.display = "";
+    };
+
+    // Hook pointer lock state change events
+    document.addEventListener("pointerlockchange", pointerlockchange, false);
+    document.addEventListener("mozpointerlockchange", pointerlockchange, false);
+    document.addEventListener(
+      "webkitpointerlockchange",
+      pointerlockchange,
+      false
+    );
+
+    document.addEventListener("pointerlockerror", pointerlockerror, false);
+    document.addEventListener("mozpointerlockerror", pointerlockerror, false);
+    document.addEventListener(
+      "webkitpointerlockerror",
+      pointerlockerror,
+      false
+    );
+
+    scope.instructions.addEventListener(
+      "click",
+      function (event) {
+        scope.instructions.style.display = "none";
+
+        // Ask the browser to lock the pointer
+        element.requestPointerLock =
+          element.requestPointerLock ||
+          element.mozRequestPointerLock ||
+          element.webkitRequestPointerLock;
+
+        if (/Firefox/i.test(navigator.userAgent)) {
+          var fullscreenchange = function (event) {
+            if (
+              document.fullscreenElement === element ||
+              document.mozFullscreenElement === element ||
+              document.mozFullScreenElement === element
+            ) {
+              document.removeEventListener(
+                "fullscreenchange",
+                fullscreenchange
+              );
+              document.removeEventListener(
+                "mozfullscreenchange",
+                fullscreenchange
+              );
+
+              element.requestPointerLock();
+            }
+          };
+
+          document.addEventListener(
+            "fullscreenchange",
+            fullscreenchange,
+            false
+          );
+          document.addEventListener(
+            "mozfullscreenchange",
+            fullscreenchange,
+            false
+          );
+
+          element.requestFullscreen =
+            element.requestFullscreen ||
+            element.mozRequestFullscreen ||
+            element.mozRequestFullScreen ||
+            element.webkitRequestFullscreen;
+
+          element.requestFullscreen();
+        } else {
+          element.requestPointerLock();
+        }
+
+        // needs to be here or these don't fire after requestPointerLock
+        element.addEventListener("mousedown", onMouseDown, false);
+        element.addEventListener("mouseup", onMouseUp, false);
+      },
+      false
+    );
+  };
+
+  // doClick is called by main.js with mouseDragOn.
+  scope.doClick = function () {
+    if (scope.enabled === false) return;
+
+    switch (scope.button) {
+      case 0:
+        scope.onLeftClick();
+        break;
+      case 2:
+        scope.onRightClick();
+        break;
+    }
+  };
+
+  var onMouseDown = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    switch (
+      event.button
+
+      //case 0: this.moveForward = true; break;
+      //case 2: this.moveBackward = true; break;
+    ) {
+    }
+
+    scope.button = event.button;
+    scope.mouseDragOn = true;
+  };
+  var onMouseUp = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    switch (
+      event.button
+
+      //case 0: this.moveForward = false; break;
+      //case 2: this.moveBackward = false; break;
+      //case 0: scope.onLeftClick(); break;
+      //case 2: scope.onRightClick(); break;
+    ) {
+    }
+
+    scope.mouseDragOn = false;
+  };
+};
---
+++ js/main.js
@@ -0,0 +1,441 @@
+if (!Detector.webgl) {
+  Detector.addGetWebGLMessage();
+  document.getElementById("container").innerHTML = "";
+}
+
+g.world = {};
+g.cubeLog = {};
+
+var fogExp2 = true;
+
+var container, stats;
+
+var camera, controls, scene, renderer;
+var previewCube;
+var mesh;
+var mat = {};
+
+g.gravity = -0.3;
+g.jumpVY = 0.13;
+g.skyColor = 0xddddff;
+
+g.cameraFOV = 100;
+g.cameraNear = 0.001;
+g.cameraFar = 200;
+
+// in blocks
+var worldWidth = 200,
+  worldDepth = 200;
+var worldHalfWidth = worldWidth / 2;
+var worldHalfDepth = worldDepth / 2;
+
+var blockType = {
+  grass: "g",
+  dirt: "d",
+  red: 0xff0000,
+  green: 0x00ff00,
+  blue: 0x0000ff,
+  white: 0xffffff,
+  black: 0x000000,
+};
+
+var clock = new THREE.Clock();
+var SECOND_MS = 1000;
+g.lastClick = -1;
+
+var maxPlacementRange = 10;
+var collideVectorStepSize = 0.2;
+
+function getPointyTarget() {
+  // Tracks down the target for destroying cubes
+  // and the previous point for constructing cubes.
+  var dir = camera.direction();
+  dir.multiplyScalar(collideVectorStepSize);
+  var dist = 0;
+  var vec = camera.position.clone();
+  var prevPoint = undefined;
+
+  while (dist < maxPlacementRange) {
+    dist += collideVectorStepSize;
+    vec.add(dir);
+    var point = [Math.round(vec.x), Math.round(vec.y), Math.round(vec.z)];
+
+    if (g.world[point] !== undefined) {
+      return [prevPoint, point];
+    }
+
+    if (point != prevPoint) {
+      prevPoint = point;
+    }
+  }
+
+  return [undefined, undefined];
+}
+
+function onLeftClick() {
+  var target = getPointyTarget()[0];
+  if (target !== undefined) {
+    if (makeCube(blockType.dirt, target)) {
+      g.cubeLog[target] = target;
+    }
+  }
+}
+
+function onRightClick() {
+  var target = getPointyTarget()[1];
+  if (target !== undefined) {
+    if (destroyCube(target)) {
+      delete g.cubeLog[target];
+    }
+  }
+}
+
+function init() {
+  container = document.getElementById("container");
+
+  scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2(g.skyColor, 0.015);
+  initMaterials();
+  initPreviewCube();
+  generateLandscape();
+
+  var aspect = window.innerWidth / window.innerHeight;
+  camera = new THREE.PerspectiveCamera(
+    g.cameraFOV,
+    aspect,
+    g.cameraNear,
+    g.cameraFar
+  );
+
+  camera.vx = 0;
+  camera.vy = 0;
+  camera.vz = 0;
+  camera.isOnGround = true;
+  camera.direction = function () {
+    // Figure out the camera viewing direction
+    var lookingAt = new THREE.Vector3(0, 0, -1);
+    lookingAt = lookingAt.applyEuler(camera.rotation, camera.eulerOrder);
+    return lookingAt;
+  };
+
+  var hasPointerLock =
+    "pointerLockElement" in document ||
+    "mozPointerLockElement" in document ||
+    "webkitPointerLockElement" in document;
+  if (hasPointerLock) {
+    controls = new THREE.PointerLockControls(camera);
+    controls.register();
+  } else {
+    alert(
+      "Mine3js requires pointer lock and your browser doesn't seem to have it :/"
+    );
+  }
+
+  camera.position.y = landscapeY(worldHalfWidth, worldHalfDepth) + 2;
+  camera.position.x = worldHalfWidth;
+  camera.position.z = worldHalfDepth;
+
+  controls.onLeftClick = onLeftClick;
+  controls.onRightClick = onRightClick;
+
+  controls.movementSpeed = 8;
+  controls.lookSpeed = 0.125;
+  controls.lookVertical = true;
+  controls.constrainVertical = true;
+  //controls.verticalMin = 1.1;
+  //controls.verticalMax = 2.2;
+
+  var ambientLight = new THREE.AmbientLight(0xcccccc);
+  scene.add(ambientLight);
+
+  var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight.position.set(1, 1, 0.5).normalize();
+  scene.add(directionalLight);
+
+  renderer = new THREE.WebGLRenderer({ clearColor: g.skyColor, clearAlpha: 1 });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  container.innerHTML = "";
+
+  container.appendChild(renderer.domElement);
+
+  //
+
+  window.addEventListener("resize", onWindowResize, false);
+
+  g.restoreFromUrl();
+
+  // some sun/moon to reach for, and mark the axis
+  makeCube(blockType.red, [0, 20, 0]);
+  makeCube(blockType.green, [100, 20, 0]);
+  makeCube(blockType.blue, [0, 20, 100]);
+}
+
+function destroyCube(point) {
+  if (g.world[point] === undefined) {
+    console.log("Tried to destroy in empty block");
+    return false;
+  }
+  if (g.world[point].blockType == blockType.grass) {
+    console.log("Destroying grass not yet implemented");
+    return false;
+  }
+
+  var obj = g.world[point];
+  scene.remove(obj);
+  //obj.dispose();
+  //obj.deallocate();
+  //obj.geometry.deallocate();
+  //obj.material.deallocate();
+  //obj.material.map.deallocate();
+  //renderer.deallocateObject( obj );
+  //renderer.deallocateTexture( texture );
+  //renderer.deallocateMaterial( material );
+
+  delete g.world[point];
+
+  return true;
+}
+
+function makeCube(type, point) {
+  if (g.world[point] !== undefined) {
+    console.log("Tried to create in occupied block");
+    return false;
+  }
+  g.world[point] = NaN; // temp val for collision test
+  if (isCollided()) {
+    console.log("Tried to create block inside body");
+    delete g.world[point];
+    return false;
+  }
+
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  var color = 0x000000;
+  if (typeof type == "number") {
+    color = type;
+  }
+
+  var material;
+  if (type == blockType.dirt) {
+    material = mat.dirt;
+  } else {
+    material = new THREE.MeshLambertMaterial({
+      color: color,
+      overdraw: true,
+      fog: false,
+      shading: THREE.FlatShading,
+    });
+  }
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = point[0];
+  mesh.position.y = point[1];
+  mesh.position.z = point[2];
+
+  // for physics
+  mesh.blockType = type;
+  g.world[point] = mesh;
+
+  scene.add(mesh);
+  return true;
+}
+
+g.numToCode = function (number) {
+  var text = Base64.fromNumber(number);
+  if (text.length < 2) {
+    return "0" + text;
+  } else {
+    return text;
+  }
+};
+
+g.worldFloor = -13;
+g.urlMarker = "#a=";
+
+g.restoreFromUrl = function () {
+  var url = window.location.href + "";
+
+  var hashLoc = url.indexOf(g.urlMarker);
+  if (hashLoc == -1) {
+    return;
+  }
+  var data = url.slice(hashLoc + g.urlMarker.length);
+  console.log("url data " + data.length);
+  console.log("data:" + data);
+
+  var coords = [];
+  for (var i = 0; i < data.length; i += 4) {
+    coords.push(Base64.toPoint(data.slice(i, i + 4)));
+  }
+
+  //console.log(coords);
+  for (var i = 0; i < coords.length; i++) {
+    // +13 was to avoid negative numbers in the Y axis
+    var t = coords[i];
+    var target = [t[0], t[1] + g.worldFloor, t[2]];
+    g.cubeLog[target] = target;
+    makeCube(blockType.dirt, target);
+  }
+};
+
+
+
+function initMaterials() {
+  var textureGrass = THREE.ImageUtils.loadTexture("img/grass.png");
+  textureGrass.magFilter = THREE.NearestFilter;
+  textureGrass.minFilter = THREE.LinearMipMapLinearFilter;
+
+  var textureDirt = THREE.ImageUtils.loadTexture("img/dirt.png");
+  textureDirt.magFilter = THREE.NearestFilter;
+  textureDirt.minFilter = THREE.LinearMipMapLinearFilter;
+
+  var textureGrassDirt = THREE.ImageUtils.loadTexture("img/grass_dirt.png");
+  textureGrassDirt.magFilter = THREE.NearestFilter;
+  textureGrassDirt.minFilter = THREE.LinearMipMapLinearFilter;
+
+  mat.grass = new THREE.MeshLambertMaterial({
+    map: textureGrass,
+    ambient: 0xbbbbbb,
+    vertexColors: THREE.VertexColors,
+  });
+  mat.dirt = new THREE.MeshLambertMaterial({
+    map: textureDirt,
+    ambient: 0xbbbbbb,
+    vertexColors: THREE.VertexColors,
+    shading: THREE.FlatShading,
+  });
+  mat.grassDirt = new THREE.MeshBasicMaterial({
+    map: textureGrassDirt,
+    ambient: 0xbbbbbb,
+    vertexColors: THREE.VertexColors,
+  });
+  mat.wire = new THREE.MeshBasicMaterial({
+    color: 0x111166,
+    wireframe: true,
+    transparent: false,
+  });
+}
+
+function initPreviewCube() {
+  var geometry = new THREE.CubeGeometry(1, 1, 1);
+  var material = mat.wire;
+
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = -1;
+  mesh.position.y = -1;
+  mesh.position.z = -1;
+
+  previewCube = mesh;
+  scene.add(mesh);
+}
+
+function updatePreviewCube() {
+  var targets = getPointyTarget();
+  var first = targets[0];
+  if (first === undefined) {
+    previewCube.visible = false;
+    return;
+  }
+  previewCube.position.x = first[0];
+  previewCube.position.y = first[1];
+  previewCube.position.z = first[2];
+  previewCube.visible = true;
+  //console.log(first);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  controls.handleResize();
+}
+
+function loadTexture(path, callback) {
+  var image = new Image();
+
+  image.onload = function () {
+    callback();
+  };
+  image.src = path;
+
+  return image;
+}
+
+function isCollided() {
+  var pos = camera.position;
+
+  // You're standing above the cube that's Math.floor and you're
+  // close to the cube that's Math.round near you.
+  var ix = Math.round(pos.x);
+  var iz = Math.round(pos.z);
+
+  var iy = Math.floor(pos.y);
+
+  if (g.world[[ix, iy, iz]] !== undefined) {
+    return true;
+  }
+
+  if (g.world[[ix, iy - 1, iz]] !== undefined) {
+    return true;
+  }
+
+  return false;
+}
+
+function physics(dt) {
+  /*
+     dt - delta time from last frame in seconds.
+    */
+  if (!controls.enabled) {
+    return;
+  }
+  var pos = camera.position;
+  camera.vy = camera.vy + g.gravity * dt;
+  camera.vy = Math.max(camera.vy, -5);
+
+  pos.y += camera.vy;
+  if (isCollided()) {
+    pos.y -= camera.vy;
+    camera.vy = 0;
+    camera.isOnGround = true;
+  }
+
+  var lookingAt = camera.direction();
+  var ddx = -lookingAt.z * camera.vx - lookingAt.x * camera.vz;
+  var ddz = lookingAt.x * camera.vx - lookingAt.z * camera.vz;
+  pos.x += ddx;
+  if (isCollided()) {
+    pos.x -= ddx;
+  }
+  pos.z += ddz;
+  if (isCollided()) {
+    pos.z -= ddz;
+  }
+
+  if (controls.mouseDragOn) {
+    if (Date.now() - g.lastClick > 0.3 * SECOND_MS) {
+      g.lastClick = Date.now();
+      controls.doClick();
+    }
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  var dt = Math.min(0.2, clock.getDelta());
+  render(dt);
+
+  stats.update();
+}
+
+function render(dt) {
+  controls.update(dt);
+  physics(dt);
+  updatePreviewCube();
+  renderer.render(scene, camera);
+}
+
+init()
+animate()
